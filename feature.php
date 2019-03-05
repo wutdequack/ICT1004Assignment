16f5b8665bd88d0c21f03514252f1ad1524aebca
wutdequack <jonathanleejuler@gmail.com>
2019-03-05 15:34:30 +0800

    Changed the arrow for the nav

---
+++ feature.php
@@ -0,0 +1,348 @@
+﻿<!DOCTYPE html>
+<!--
+    Name: Skylith - Viral & Creative Multipurpose HTML Template
+    Version: 1.0.3
+    Author: nK, unvab
+    Website: https://nkdev.info/, http://unvab.com/
+    Purchase: https://themeforest.net/item/skylith-viral-creative-multipurpose-html-template/21214857?ref=_nK
+    Support: https://nk.ticksy.com/
+    License: You must have a valid license purchased only from ThemeForest (the above link) in order to legally use the theme for your project.
+    Copyright 2018.
+-->
+    
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Mei Heong Yuan Dessert - Feature</title>
+
+    <meta name="description" content="Feature">
+    <meta name="keywords" content="dessert, food, asian, features">
+    <meta name="author" content="Lee Jun Ming">
+
+    <link rel="icon" type="image/png" href="assets/images/mhylogo.png">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- START: Styles -->
+    
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i%7cWork+Sans:400,500,700%7cPT+Serif:400i,500i,700i" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/vendor/bootstrap/dist/css/bootstrap.min.css">
+
+    <!-- FontAwesome -->
+    <script defer src="assets/vendor/fontawesome-free/js/all.js"></script>
+    <script defer src="assets/vendor/fontawesome-free/js/v4-shims.js"></script>
+
+    <!-- Stroke 7 -->
+    <link rel="stylesheet" href="assets/vendor/pixeden-stroke-7-icon/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+
+    <!-- Flickity -->
+    <link rel="stylesheet" href="assets/vendor/flickity/dist/flickity.min.css">
+
+    <!-- Photoswipe -->
+    <link rel="stylesheet" href="assets/vendor/photoswipe/dist/photoswipe.css">
+    <link rel="stylesheet" href="assets/vendor/photoswipe/dist/default-skin/default-skin.css">
+
+    <!-- JustifiedGallery -->
+    <link rel="stylesheet" href="assets/vendor/justifiedGallery/dist/css/justifiedGallery.min.css">
+
+    <!-- Skylith -->
+    <link rel="stylesheet" href="assets/css/skylith.css">
+
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="assets/css/mhy-custom.css">
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+    
+    <!-- END: Styles -->
+
+    <!-- jQuery -->
+    <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="assets/js/mhy-custom.js"></script>
+    
+</head>
+
+<!--
+    Additional Classes:
+        .nk-bg-gradient
+-->
+<body>
+    
+<!--Code to insert external nav page-->
+<?php include('nav.inc.php'); ?>
+
+
+
+<!--
+START: Main Content
+
+Additional Classes:
+    .nk-main-dark
+-->
+<div class="nk-main">
+
+<!-- START: Header Title 
+<!--
+    Additional Classes:
+        .nk-header-title-sm
+        .nk-header-title-md
+        .nk-header-title-lg
+        .nk-header-title-xl
+        .nk-header-title-full
+        .nk-header-title-parallax
+        .nk-header-title-parallax-content
+        .nk-header-content-center-center
+        .nk-header-content-center-left
+        .nk-header-content-center-right
+        .nk-header-content-top-center
+        .nk-header-content-top-left
+        .nk-header-content-top-right
+        .nk-header-content-bottom-center
+        .nk-header-content-bottom-left
+        .nk-header-content-bottom-right
+-->
+<div class="nk-header-title nk-header-title-lg nk-header-title-parallax-content nk-header-content-center-center">
+    <div class="bg-image bg-image-parallax">
+        <!-- Credits to Mr Kathiravan Manoharan for the beautiful shot
+            http%3A%2F%2Fwww.kathiravan-manoharan.com%2F2017%2F07%2Fwanderers-diary-tangyuan-and-mei-heong.html&psig=AOvVaw1oiqVAri4UuNKHIm4So0zR&ust=1550822147084807-->
+        <img src="assets/images/featured-gallery.jpg" alt="" class="jarallax-img">
+        <div class="bg-image-overlay" style="background-color: rgba(255, 255, 255, 0.2);"></div>
+    </div>
+    
+    <div class="nk-header-content">
+        <div class="nk-header-content-inner">
+            <div class="container">
+
+            </div>
+        </div>
+    </div>
+</div>
+ <!-- END: Header Title -->
+
+
+<!-- START: Feature -->
+<div class="container">
+    
+    <!-- START: Filter -->
+    <div class="nk-pagination nk-pagination-nobg nk-pagination-center">
+        <a href="#nk-toggle-filter"><span class="nk-icon-squares"></span></a>
+    </div>
+    <ul class="nk-isotope-filter">
+        <li class="active" data-filter="*">All</li>
+        <li data-filter="Media">Media Release</li>
+        <li data-filter="Awards">Awards</li>
+    </ul>
+    <!-- END: Filter -->
+    
+    <div class="nk-portfolio-list nk-isotope nk-isotope-1-cols nk-popup-gallery">
+    <div class="nk-isotope-item" data-filter="*">
+        <p>
+            In recent years, our hard work and effort have paid off.
+            This has been the result of your lovely recommendations. Do continue to spread the word about us to your friends and families!
+        </p>
+    </div>
+        
+    <!-- START: Media -->
+    <div class="nk-isotope-item" data-filter="Media">
+        <h1>Featured Videos</h1>
+        <p>
+            We are especially honoured to have been featured on several Japanese TV shows!
+        </p>
+        
+            <div id="carouselIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+                
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <iframe class="d-block w-100" src="https://player.vimeo.com/video/125676867?color=ffffff&byline=0&portrait=0" width="650" height="435" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div class="carousel-item">
+                        <iframe class="d-block w-100" src="https://player.vimeo.com/video/105122371?color=ffffff&byline=0&portrait=0" width="650" height="435" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div class="carousel-item">
+                        <iframe class="d-block w-100" src="https://player.vimeo.com/video/91467302?color=ffffff&byline=0&portrait=0" width="650" height="435" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div class="carousel-item">
+                        <iframe class="d-block w-100" src="https://player.vimeo.com/video/91242536?color=ffffff&byline=0&portrait=0" width="650" height="435" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        
+        <div class="nk-gap-2"><hr></div>
+        
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Featured Articles</h1>
+                <p>
+                    We have been featured on several articles, including the Singapore Excellence Award and SINGA NEWS. Below are two screenshots of such.
+                </p>
+                <p>Click on the articles below to read on!</p>
+            </div>
+            <div class="col-lg-6">
+                <figure style="display:block; text-align:center;">
+                    <a href="assets/articles/MHY.pdf" target="_blank">
+                        <img src="assets/images/article-MHY.png" width="350" height="450" />
+                    </a>
+                </figure>
+            </div>
+            <div class="col-lg-6">
+                <figure style="display:block; text-align:center;">
+                    <a href="assets/articles/singa_news_article.pdf" target="_blank">
+                        <img src="assets/images/article-singa_news.png" width="350" height="450" />
+                    </a>
+                </figure>
+            </div>
+        </div>
+        <div class="nk-gap-2"><hr></div>
+    </div>
+    <!-- END: Media -->
+    
+    <!-- START: Awards -->
+    <div class="nk-isotope-item" data-filter="Awards">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Awards & Recognitions</h1>
+                <p>
+                    Over the years of establishment, Mei Heong Yuen Dessert has been featured in various press and magazines, both locally and abroad. Notable ones are namely, <strong>"Lian He Wan Bao" 联合晚报</strong>; <strong>"Sin Ming Ri Bao" 新明日报</strong>; <strong>Weekend Today</strong>; <strong>"Guang Ming Ri Bao" 光明日报</strong> and Japanese newspaper <strong>“Shin Nichi Ho”</strong>.
+                </p>
+                <ul>
+                    <li>Awarded the BEST FOOD AWARDS 2009/2010 by "The Green Book"</li>
+                    <li>Recommended dessert outlet under the show “Snack Attack 2" by MediaCorp Channel 8</li>
+                    <li>As one of the favourite restaurants in Asia, we are recognised as a recommended choice in the Singapore “Ice Kachang” category as part of "CNNGo’s Best Eats 2010 Awards"</li>
+                </ul>
+            </div>
+                <div class="col-lg-4">
+                    <h2>2012</h2>
+                    <figure>
+                        <img class="img-modal" src="assets/images/awards-logo_seamain.jpg" alt="seamain_logo_award" data-toggle="modal" data-target="#awards-modal" width="200" height="280" />
+                    </figure>
+                </div>
+            <div class="col-lg-4">
+                <section>
+                    <h2>2010</h2>
+                    <figure>
+                        <img class="img-modal" src="assets/images/awards-CNNGo.jpg" alt="CNNGo_award" data-toggle="modal" data-target="#awards-modal" width="200" height="135" />
+                    </figure>
+                    <figure>
+                        <img class="img-modal" src="assets/images/awards-TheGreenBook.jpg" alt="TheGreenBook_award" data-toggle="modal" data-target="#awards-modal" width="200" height="135" />
+                    </figure>
+                </section>
+            </div>
+            <div class="col-lg-4">
+                <section>
+                    <h2>2008</h2>
+                    <figure>
+                        <img class="img-modal" src="assets/images/awards-SnackAttack2.jpg" alt="snackattack-award" data-toggle="modal" data-target="#awards-modal" width="200" height="280" />
+                    </figure>
+                </section>
+            </div>
+            
+            <!-- Modal -->
+            <div class="modal fade" id="awards-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-body" style="text-align: center;">
+                            <img class="img-responsive" src="" style="width:70%;"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <!-- END: Awards -->
+    
+    </div>
+    <div class="nk-gap-2"></div>
+<!-- END: Feature -->
+</div>
+
+
+<!-- START: Pagination -->
+<div class="nk-pagination nk-pagination-center">
+    <a href="#">Return Back To Top</a>
+</div>
+<!-- END: Pagination -->
+
+</div>
+<!-- END: Main Content -->
+
+    
+<!--Code to insert external footer page-->
+<?php include('footer.inc.php'); ?>
+
+    
+<!-- START: Scripts -->
+
+<!-- Object Fit Polyfill -->
+<script src="assets/vendor/object-fit-images/dist/ofi.min.js"></script>
+
+<!-- ImagesLoaded -->
+<script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+
+<!-- GSAP -->
+<script src="assets/vendor/gsap/src/minified/TweenMax.min.js"></script>
+<script src="assets/vendor/gsap/src/minified/plugins/ScrollToPlugin.min.js"></script>
+
+<!-- Popper -->
+<script src="assets/vendor/popper.js/dist/umd/popper.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Sticky Kit -->
+<script src="assets/vendor/sticky-kit/dist/sticky-kit.min.js"></script>
+
+<!-- Jarallax -->
+<script src="assets/vendor/jarallax/dist/jarallax.min.js"></script>
+<script src="assets/vendor/jarallax/dist/jarallax-video.min.js"></script>
+
+<!-- Flickity -->
+<script src="assets/vendor/flickity/dist/flickity.pkgd.min.js"></script>
+
+<!-- Isotope -->
+<script src="assets/vendor/isotope-layout/dist/isotope.pkgd.min.js"></script>
+
+<!-- Photoswipe -->
+<script src="assets/vendor/photoswipe/dist/photoswipe.min.js"></script>
+<script src="assets/vendor/photoswipe/dist/photoswipe-ui-default.min.js"></script>
+
+<!-- JustifiedGallery -->
+<script src="assets/vendor/justifiedGallery/dist/js/jquery.justifiedGallery.min.js"></script>
+
+<!-- Jquery Validation -->
+<script src="assets/vendor/jquery-validation/dist/jquery.validate.min.js"></script>
+
+<!-- Hammer.js -->
+<script src="assets/vendor/hammerjs/hammer.min.js"></script>
+
+<!-- NanoSroller -->
+<script src="assets/vendor/nanoscroller/bin/javascripts/jquery.nanoscroller.js"></script>
+
+<!-- Keymaster -->
+<script src="assets/vendor/keymaster/keymaster.js"></script>
+
+
+<!-- Skylith -->
+<script src="assets/js/skylith.min.js"></script>
+<script src="assets/js/skylith-init.js"></script>
+<!-- END: Scripts -->
+
+    
+</body>
+</html>
